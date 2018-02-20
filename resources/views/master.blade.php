64ff3dbbffb97e25c30039d838b75456281e193c
Jake V <jacob@sadsoft.com>
2018-02-19 22:01:13 -0800

    More upgrading to Bootstrap 4

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,26 +4,37 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Scotch.io GuestBook</title>
+    <title>GuestBook</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-findcond">
+            <a class="navbar-brand" href="{{ route('home') }}">GuestBook</a>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="nav-item">
+                    <a href="{{ route('sign') }}">Sign the GuestBook</a>
+                </li>
+            </ul>
+        </nav>
+        {{--
+        <nav class="navbar navbar-findcond">
             <div class="container">
                 <div class="navbar-header">
                     <a class="navbar-brand" href="{{ route('home') }}">GuestBook</a>
                 </div>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <div class="collapse navbar-collapse" id="navbar">
                     <ul class="nav navbar-nav navbar-right">
-                        <li class="active">
+                        <li class="nav-item">
                             <a href="{{ route('sign') }}">Sign the GuestBook</a>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
+        --}}
         @yield('content')
     </div>
     <script src="{{ mix('js/app.js') }}"></script>
